12dbc34de9d5f9574de04bbbcde0aa5ab7e28512
Andredev66 <unisocorroandres@gmail.com>
2026-09-19 10:53:18 -0400

    correciones linux

--- Main.py
+++ Main.py
@@ -1972,10 +1972,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Agregar Producto")
         dialog.geometry("300x250")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -2071,10 +2068,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title(f"Agrupar: {desc}")
         dialog.geometry("350x200")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -2216,10 +2210,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Procesar Pago")
         dialog.geometry("800x700")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -2369,10 +2360,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.minsize(460, 460)
         dialog.resizable(True, True)
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
 
@@ -2626,10 +2614,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Nuevo Producto")
         dialog.geometry("500x550")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -2797,10 +2782,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Editar Producto")
         dialog.geometry("500x550")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -2958,10 +2940,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Nuevo Cliente")
         dialog.geometry("500x500")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -3093,10 +3072,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Editar Cliente")
         dialog.geometry("500x500")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -3254,10 +3230,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Nuevo Cliente Rápido")
         dialog.geometry("400x300")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
@@ -3311,10 +3284,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Buscar Cliente")
         dialog.geometry("450x400")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
 
@@ -4205,10 +4175,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.geometry(f"{min(980, max(560, int(screen_w * 0.82)))}x{min(900, max(440, int(screen_h * 0.82)))}")
         dialog.minsize(560, 440)
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
 
@@ -4344,10 +4311,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title("Registrar Pago")
         dialog.geometry("840x220")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
 
@@ -5890,10 +5854,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title(f"Detalle de Producto: {product['name']}")
         dialog.geometry("800x800")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         dialog.bind("<Escape>", lambda e: safe_destroy(dialog))
@@ -5946,10 +5907,7 @@ class BillingSystem(ctk.CTkToplevel):
         dialog.title(f"Detalle de Cliente: {client['name']}")
         dialog.geometry("500x600")
         dialog.transient(self)
-<<<<<<< HEAD
         dialog.update_idletasks()
-=======
->>>>>>> 53fe27721c7ed380feae37c74d91e5d93beef491
         dialog.grab_set()
         fade_in_window(dialog)
         
